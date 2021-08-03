82a1fe6c04ae19ea0a2e624250943fc5503d9ae0
JocimaraMartins <jocimara1995santos@gmail.com>
2021-08-03 15:25:39 -0300

    commit

---
+++ pythonProject/jogoteca.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+@app.route('/inicio')
+
+def ola():
+    jogo1 = Jogo('Super Mario', 'Acao', 'SNES')
+    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+    jogo3 = Jogo('Truco', 'Tabuleiro','Cartas')
+    lista = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo='Jogos',
+                           jogos=lista)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+
+app.run()
